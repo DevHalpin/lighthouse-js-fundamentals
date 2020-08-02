083dae09efc8c112893d8a5fa5b7b35d297640f2
DevHalpin <echidnablade@hotmail.com>
2020-08-02 19:27:50 +0000

    Added return function

--- hello.js
+++ hello.js
@@ -2,6 +2,13 @@ const sayHello = function (name) {
   console.log('Hello, ' + world);
 }
 
+const returnSayHello = function (name) {
+  return 'Hello, ' + name;
+}
+
+const greeting = returnSayHello('John');
+console.log(greeting);
+
 sayHello('Caliban');
 sayHello('Miranda');
 sayHello('Ferdinand');
